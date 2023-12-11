43ea1ec0d2097f6e52f68b89f2cf159ca6b547fe
Rebeca Felix <97916363+Rebeca-Felix@users.noreply.github.com>
2023-12-11 08:51:59 -0300

    Update Explorando Sintaxe e Tipos de Dados.cs

--- Desafios/Explorando Sintaxe e Tipos de Dados.cs
+++ Desafios/Explorando Sintaxe e Tipos de Dados.cs
@@ -8,4 +8,33 @@
 //Construa uma mensagem de saída formatada corretamente, como nessa estrutura: ' registroNome + ", verifique o email: " + registroEmail + " para ativar." '
 
 //Exemplos
-//A tabela abaixo apresenta exemplos com alguns dados de entrada e suas respectivas saídas esperadas. Certifique-se de testar seu programa com esses exemplos e com outros casos possíveis.
+//A tabela abaixo apresenta exemplos com alguns dados de entrada e suas respectivas saídas esperadas. 
+//Certifique-se de testar seu programa com esses exemplos e com outros casos possíveis.
+
+// Entrada: anavaz@email.com, Ana, 2323 | Saída:	Ana, verifique o email: anavaz@email.com para ativar.
+// Entrada: joaocarlos@email.com, Joao, 3232 | Saída:	Joao, verifique o email: joaocarlos@email.com para ativar.
+// Entrada: shaula@email.com, Shaula, 8989 | Saída:	Shaula, verifique o email: shaula@email.com para ativar.
+
+// Resposta
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+Console.WriteLine( );
+String email = Console.ReadLine();
+
+Console.WriteLine();
+String nome = Console.ReadLine();
+
+ Console.WriteLine();
+String senha = Console.ReadLine();
+
+Console.WriteLine( nome + ", verifique o email: " + email + " para ativar.");
+
+
+    }
+}
